f9cedabfade2c3006f8f3c844f0bb1bc9d4da2fe
Tanner Lancaster <saltillokid11@yahoo.com>
2019-12-29 12:39:54 -0500

    finished 3 part 1, added morgan, custom request logger, and api endpoints

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan');
 
-app.use(bodyParser.json())
-app.use(cors())
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+};
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(requestLogger);
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+});
+
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
+        tokens['body'](req, res),
+    ].join(' ')
+}));
 
 let persons = [
     {
@@ -38,7 +65,7 @@ let persons = [
 
 const generateId = () => {
     return Math.floor(Math.random() * 100) + 10;
-}
+};
 
 app.get('/', (req, res) => {
     res.send('<h1>Part 3</h1>');
@@ -78,25 +105,26 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({
             error: 'content missing'
         })
-    }
-    
-//?????????????????????????????????? why is this always true?
-    if(persons.map(p => p.name === body.name)){
-        return res.status(400).json({
-            error: 'name already exists'
-        })
-    }
+    };
+
+    persons.map(p => {
+        if(p.name === body.name){
+            return res.status(400).json({
+                error: 'name already exists'
+            })
+        }
+    });
 
     const person = {
         name: body.name,
         number: body.number,
         id: generateId(),
-    }
+    };
 
     persons = persons.concat(person)
 
     res.json(person)
-})
+});
 
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -104,7 +132,13 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== id)
 
     res.status(204).end();
-})
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+};
+
+app.use(unknownEndpoint);
 
 
 const PORT = process.env.PORT || 3001
